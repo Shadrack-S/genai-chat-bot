478a1df83cdf0a41a149bedd7cb6be4c3528ce18
Shadrack S <shadrackshibu1@yahoo.com>
2025-09-14 07:14:21 +0530

    implemeneted SideBar *basic setup for react vite * react-router integration * Side Bar design implemenetation * tailwindcss and moment js implementation in side bar *Google font implementataion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import SideBar from './components/SideBar.jsx'
+import { Route, Routes } from "react-router";
+import ChatBox from './components/ChatBox.jsx';
+import Login from './pages/Login.jsx';
+import { assets } from './assets/assets.js';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [isMenuOpen,setIsMenuOpen]= useState(false)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   <>
+   {!isMenuOpen && <img src ={assets.menu_icon} 
+   className='absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert' onClick={()=>setIsMenuOpen(true)}/>}
+   <div className='dark:bg-gradient-to-b from-[#242124] to-[#000000] 
+   dark:text-white'>
+    <div className='flex h-screen w-screen'>
+    <SideBar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen}/>
+    <Routes>
+      <Route path='/' element={<ChatBox/>}/>
+      <Route path='/login' element={<Login/>}/>
+      
+    </Routes>
+   </div>
+   </div>
+   </>
   )
 }
 
---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,67 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import { dummyChats, dummyUserData } from "../assets/assets";
+
+
+const AppContext = createContext()
+
+export const AppContextProvider = ({ children }) => {
+    const navigate = useNavigate()
+    const [user, setUser] = useState(null)
+    const [chats, setChats] = useState([])
+    const [selectedChat, setSelectedChat] = useState(null);
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light')
+
+    const fetchUser = async () => {
+        setUser(dummyUserData)
+    }
+
+    const fetchUsersChats = async ( )=>{
+        setChats(dummyChats)
+        setSelectedChat(dummyChats[0])
+    }
+
+    // To set theme in local storage
+    useEffect(()=>{
+        if(theme === 'dark'){
+            document.documentElement.classList.add('dark');
+        }else{
+            document.documentElement.classList.remove('dark');
+        }
+
+    },[theme])
+
+    // For loading chat when user is logged in
+    useEffect(()=>{
+        if(user){
+            fetchUsersChats()
+        }else{
+            setChats([])
+            setSelectedChat(null)
+        }
+    },[user])
+
+    // To check if user is logged in or not
+    useEffect(() => {
+        fetchUser()
+    }, [])
+    const value = {
+        navigate,
+        user,
+        setUser,
+        chats,
+        setChats,
+        selectedChat,
+        setSelectedChat,
+        theme,
+        setTheme,
+        fetchUser
+    }
+    return (
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useAppContext = () => useContext(AppContext)
